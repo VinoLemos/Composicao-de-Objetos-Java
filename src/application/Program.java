71bd917944cdd3945e604831acfd1c5c225b0288
VinoLemos <vlemosdeoliveira@gmail.com>
2021-09-01 15:23:54 -0300

    Criadas classes Product, Program e OrderItem

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// Produto TV com preço unitário de 1000.00
+		Product p = new Product("TV", 1000.00);
+	}
+
+}
